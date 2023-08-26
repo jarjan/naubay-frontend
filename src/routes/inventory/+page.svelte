5a2fde8e925ba8167a2483743588895229b93bdb
Merey Zholdas <merey.90@gmail.com>
2023-08-26 15:07:29 +0200

    conclude the day form

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Counter from '../Counter.svelte';
   import products from "$lib/data/products.json";
 </script>
 
@@ -15,11 +16,42 @@
     </tr>
     {/each}
   </table>
+  <h1>Conclude the day:</h1>
+  <form class="conclude__form">
+    <!-- list of sold items for the day -->
+    {#each products as product}
+    <div class="conclude_item"><span class="conclude_item_name">{product.name.toUpperCase()}:</span><Counter /></div>
+    {/each}
+    
+    <button class="conclude__button" type="submit">Conclude</button>
+  </form>
 </div>
 
 <style>
   .inventory__table {
     margin: auto;
     text-align: center;
+  }
+
+  .conclude__form {
+    display: flex;
+    flex-direction: column;
+    width: 800px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .conclude_item {
+    display: flex;
+    width: 100%;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .conclude_item_name {
+    flex-grow: 1;
+    font-size: 2rem;
+    margin-right: 1rem;
+    text-align: right;
   }
 </style>
