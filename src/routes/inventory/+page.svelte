71592a723b78686cc6091d1d8582fff996562a4c
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-26 20:28:23 +0200

    add categories to inventory page

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -10,6 +10,7 @@
   <table class="inventory__table">
     <tr>
       <th></th>
+      <th>category</th>
       <th>name</th>
       <th>price</th>
       <th>quantity</th>
@@ -17,7 +18,8 @@
     {#each data.products as product}
     <tr>
       <td><img class="inventory__product_img" src="{product.image}" alt="{product.name}" /></td>
-      <td>{product.name}</td>
+      <td>{product.category}</td>
+      <td><span class="inventory__product_name">{product.name}</span></td>
       <td>€{product.price}</td>
       <td>
         <Counter on:update={(event) => console.log(`${product.name}: ${event.detail}`)} />
@@ -45,34 +47,43 @@
     height: 60px;
     transition: transform 0.2s ease-in-out;
   }
+  .inventory__product_name {
+    max-width: 150px;
+    display: inline-block;
+  }
+
   .inventory__product_img:hover {
     transform: scale(2.5);
   }
 
   .inventory__table tr {
-    padding: 10px;
     transition: background-color 0.2s ease-in-out;
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   }
   .inventory__table tr:has(> :not(th)):hover {
     background-color: var(--color-bg-2);
   }
+  .inventory__table td {
+    padding: 10px;
+  }
 
 
   @media (max-width: 600px) {
-    .inventory__table tr th:nth-child(4) {
+    .inventory__table td {
+      padding: 0;
+    }
+    .inventory__table tr th:nth-child(5) {
       display: none;
     }
     .inventory__table tr {
       position: relative;
     }
-    .inventory__table tr td:nth-child(3) {
-      vertical-align: top;
-
-    }
     .inventory__table tr td:nth-child(4) {
+      vertical-align: top;
+    }
+    .inventory__table tr td:nth-child(5) {
       position: absolute;
-      bottom: 10px;
+      bottom: 4px;
       right: 0;
       padding: 0;
     }
