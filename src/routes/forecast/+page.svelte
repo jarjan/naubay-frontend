18ee4bd37cc5f3e67977c4c3a2af108f470b9a33
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-26 12:42:53 +0200

    Update +page.svelte

--- src/routes/forecast/+page.svelte
+++ src/routes/forecast/+page.svelte
@@ -23,9 +23,42 @@
   );
 </script>
 
-<div>
-  forecast chart goes here
+<div class="forecast">
+  <div class="forecast__filter">
+    <select>
+      <option>all</option>
+      <option>power bread</option>
+      <option>plain bread</option>
+      <option>jam</option>
+      <option>ice coffe</option>
+      <option>croissant</option>
+      <option>ice coffe latter</option>
+      <option>tiramisu croissant</option>
+      <option>cacao deep</option>
+      <option>pain au chocolat</option>
+      <option>almond croissant</option>
+      <option>ice milk tea</option>
+      <option>gateau chocolat</option>
+      <option>baguette</option>
+      <option>cheese cake</option>
+      <option>lemon ade</option>
+      <option>orange pound</option>
+      <option>wiener</option>
+      <option>valina latte</option>
+      <option>berry ade</option>
+      <option>tiramisu</option>
+      <option>merinque cookies</option>
+    </select>
+  </div>
 
 
   <Line {data} options={{ responsive: true }} />
 </div>
+
+
+<style>
+.forecast__filter {
+  text-align: right;
+  margin: 20px;
+}
+</style>
