dc222f1579a9b88d74014783d5ba4608c1340d50
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-26 14:18:57 +0200

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,10 +2,12 @@
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
-export async function load({ params }) {
+export async function load({ fetch, url }) {
   let content = "Loading...";
 
-  await fetch("https://naubay.onrender.com/")
+  const path = url.searchParams.get("q") || "";
+
+  await fetch(`https://naubay.onrender.com/${path}`)
     .then((res) => res.json())
     .then((data) => {
       content = data.kez;
