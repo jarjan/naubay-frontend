73ec64928ebe39d920032fca8b1887c8c368c1a5
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-27 14:00:40 +0200

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,23 +1,26 @@
+import {redirect} from '@sveltejs/kit';
+
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
 export const ssr = false;
 
 export async function load({ fetch, url }) {
-  let state = "loading";
-  let products = [];
-  await fetch(`https://naubay.onrender.com/products`)
-    .then((res) => res.json())
-    .then((data) => {
-      products = data;
-      state = "loaded";
-    })
-    .catch(() => {
-      state = "error";
-    });
+  throw redirect('/order');
+  // let state = "loading";
+  // let products = [];
+  // await fetch(`https://naubay.onrender.com/products`)
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     products = data;
+  //     state = "loaded";
+  //   })
+  //   .catch(() => {
+  //     state = "error";
+  //   });
 
-  return {
-    state,
-    products,
-  };
+  // return {
+  //   state,
+  //   products,
+  // };
 }
