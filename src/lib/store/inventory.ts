5862c28465ac4d47f95c821c436b957232a76eb9
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-27 11:08:15 +0200

    persist inventory

--- src/lib/store/inventory.ts
+++ src/lib/store/inventory.ts
@@ -6,7 +6,7 @@ const initialValue = browser
   ? JSON.parse(window.localStorage.getItem("inventoryDB")) ?? defaultValue
   : defaultValue;
 
-const db = writable<Record<string, string>>(initialValue);
+const db = writable<Record<string, number>>(initialValue);
 
 db.subscribe((value) => {
   if (browser) {
--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -3,7 +3,7 @@
   import db from '$lib/store/inventory'
 
   function handleUpdate(name: string, count: string) {
-    db.set({[name]: count})
+    db.set({[name]: Number(count)})
   }
 
   export let data;
@@ -12,7 +12,7 @@
 <div class="inventory">
   <h1>Manage your inventory here</h1>
 
-  <table class="inventory__table">
+  <table class="product__table">
     <tr>
       <th></th>
       <th>category</th>
@@ -22,12 +22,12 @@
     </tr>
     {#each data.products as product}
     <tr>
-      <td><img class="inventory__product_img" src="{product.image}" alt="{product.name}" /></td>
+      <td><img class="product__img" src="{product.image}" alt="{product.name}" /></td>
       <td>{product.category}</td>
-      <td><span class="inventory__product_name">{product.name}</span></td>
+      <td><span class="product__name">{product.name}</span></td>
       <td>€{product.price}</td>
       <td>
-        <Counter count={Number($db[product.name] || '0')} on:update={(event) => handleUpdate(product.name, event.detail)} />
+        <Counter count={$db[product.name] || 0} on:update={(event) => handleUpdate(product.name, event.detail)} />
       </td>
     </tr>
     {/each}
@@ -36,64 +36,66 @@
 </div>
 
 <style>
-  .inventory__table {
-    margin: auto;
-    text-align: center;
-    border-collapse: collapse;
-  }
 
   .inventory__submit {
     display: block;
     margin: 30px auto;
   }
 
-  .inventory__product_img {
+
+  .product__table {
+    margin: auto;
+    text-align: center;
+    border-collapse: collapse;
+  }
+
+  .product__img {
     vertical-align: middle;
     height: 60px;
     transition: transform 0.2s ease-in-out;
   }
 
-  .inventory__product_name {
+  .product__name {
     max-width: 150px;
     display: inline-block;
   }
 
-  .inventory__product_img:hover {
+  .product__img:hover {
     transform: scale(2.5);
   }
 
-  .inventory__table tr {
+  .product__table tr {
     transition: background-color 0.2s ease-in-out;
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   }
 
-  .inventory__table tr:has(> :not(th)):hover {
+  .product__table tr:has(> :not(th)):hover {
     background-color: var(--color-bg-2);
   }
 
-  .inventory__table td {
+  .product__table td {
     padding: 10px;
   }
 
 
   @media (max-width: 600px) {
-    .inventory__table td {
+    .product__table td {
       padding: 0;
     }
 
-    .inventory__table tr th:nth-child(5) {
+    .product__table tr th:nth-child(5) {
       display: none;
     }
 
-    .inventory__table tr {
+    .product__table tr {
       position: relative;
     }
 
-    .inventory__table tr td:nth-child(4) {
+    .product__table tr td:nth-child(4) {
       vertical-align: top;
     }
 
-    .inventory__table tr td:nth-child(5) {
+    .product__table tr td:nth-child(5) {
       position: absolute;
       bottom: 4px;
       right: 0;
